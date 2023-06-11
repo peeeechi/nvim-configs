0f72e75dd313dc8ff3825e00642edf242780b0f9
Satoshi Inoue <is5905@paltac.co.jp>
2023-06-11 17:12:15 +0900

    mod status line theme

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,6 +1,11 @@
 ---@type ChadrcConfig 
  local M = {}
- M.ui = {theme = 'tokyodark'}
+ M.ui = {
+    theme = 'catppuccin',
+    statusline = {
+      theme = "vscode_colored",
+    },
+ }
  M.mappings = require "custom.mappings"
  M.plugins = "custom.plugins"
  return M

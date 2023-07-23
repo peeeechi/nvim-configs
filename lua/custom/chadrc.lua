492cf5ddd9ce7ed7949e73ae29dde1d36372e78e
S.Inoue <satoshi.o4o4.inoue@gmail.com>
2023-07-23 10:24:00 +0900

    コメントの色を変更

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,10 +1,15 @@
 ---@type ChadrcConfig 
  local M = {}
  M.ui = {
-    theme = 'catppuccin',
+    theme = 'ayu_dark',
     statusline = {
       theme = "vscode_colored",
     },
+  hl_override = {
+    Comment = {
+      fg = "#008800",
+    },
+  },
  }
  M.mappings = require "custom.mappings"
  M.plugins = "custom.plugins"

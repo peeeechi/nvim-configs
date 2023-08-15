79b02fe1eebdd9f47c030a0f3974e7973191d518
S.Inoue <satoshi.o4o4.inoue@gmail.com>
2023-08-15 20:30:16 +0900

    set clipboard settings

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
 require("base")
 require("maps")
 require("highlights")
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+local is_mac = has "macuix"
+local is_win = has "win32"
+
+if is_mac then
+  require('macos')
+elseif is_win then
+  require('windows')
+else
+  print('linux')
+  vim.opt.clipboard = "unnamedplus"
+end
+
